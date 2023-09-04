688cb89864865b35664b54bd8bafab856b2e0e80
splorange12 <trentw25@frontier.com>
2023-09-04 15:18:17 -0400

    tile fixed

--- modmain.lua
+++ modmain.lua
@@ -25,7 +25,7 @@ AddClassPostConstruct("widgets/mapwidget", function(self)
 			local rifty
 			for i = (GLOBAL.TheWorld.Map:GetSize())*-4, w, 4 do 
 				for j = (GLOBAL.TheWorld.Map:GetSize())*-4, h, 4 do
-					if (GLOBAL.TheWorld.Map:GetTileAtPoint(i, 0, j) == 263) then -- GLOBAL.TheWorld.Map:GetTileAtPoint(i, 0, j) == 263 or shadow rift type
+					if (GLOBAL.TheWorld.Map:GetTileAtPoint(i, 0, j) == 265) then -- GLOBAL.TheWorld.Map:GetTileAtPoint(i, 0, j) == 263 or shadow rift type
 						--print("RIFT TURF DETECTED AT COORDINATE ("..i..","..j..")")
 						riftx = i
 						rifty = j
